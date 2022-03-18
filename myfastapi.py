b856a6d184f61838cd6b6590f18212ca12a28394
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-03-18 15:06:29 +0600

    Both path and query

--- myfastapi.py
+++ myfastapi.py
@@ -17,12 +17,22 @@ students = {
 def index():
     return {"Name": "Badhon"}
 
+#path parameter
 @app.get("/get-data/{student_id}")
 def get_data(student_id: int = Path(None, description="Id must be in student", gt=0)):
     return students[student_id]
 
+#query parameter
 @app.get("/getbyname")
 def get_data(name: str):
+    for student_id in students:
+        if students[student_id]["Name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found"}
+
+#query and path parameter
+@app.get("/getboth/{student_id}")
+def get_data(student_id: int, name: str):
     for student_id in students:
         if students[student_id]["Name"] == name:
             return students[student_id]
