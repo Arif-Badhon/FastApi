c23a37f1acbb86032dc4887b1e763a83175c2647
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-03-18 10:30:50 +0600

    added student

--- myfastapi.py
+++ myfastapi.py
@@ -4,7 +4,19 @@ from fastapi import FastAPI
 #start fastapi
 app = FastAPI()
 
+#Dummy Data
+students = {
+    1 : {
+        "Name":"Badhon",
+        "age": 28,
+        "designation": "Data Specialist"
+    }
+}
 #modules
 @app.get("/")
 def index():
     return {"Name": "Badhon"}
+
+@app.get("/get-data/{student_id}")
+def get_data(student_id: int):
+    return students[student_id]
